6258cebb35d87aec6b662167bc10f1e07761fefe
azabet <azabet@gmail.com>
2014-10-04 10:46:49 -0400

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -3,14 +3,24 @@
 
 ## Clustering Analysis of Dopant Distribution
 
-# ___Goal:___ Analysis of dopant clustering in graphene, a novel 2D material. <br>
-# ___Data:___ Dopant coordinates (in nanometers) and sublattice positions (_A_ or _B_) -- data from original research. <br>
-# ___Methods:___ Classification using <u>_Logistic Regression_</u> and <u>_Support Vector Machines_</u>, and unsupervised clustering using a personally developed model implementing <u>_Spatial Autocorrelation Analysis_</u>. <br>
-# ___Outcome:___ Discovered large-scale segregation of dopant domains in graphene which led to the discovery of a new mechanism of doping.  The autocorrelation model was able to detect an unknown number of clusters, unlike logistic regression or SVM in which the number of clusters should be pre-specified.  The model also achieved detection of nonlinear domain boundaries.  Results are published in [JACS 136, 1391 (2014)](http://dx.doi.org/10.1021/ja408463g).  
+## Goal: Analysis of dopant clustering in graphene, a novel 2D material.
+## Data: Dopant coordinates (in nanometers) and sublattice positions (A or B) -- data from original research.
+## Methods: Classification using Logistic Regression and Support Vector Machines, and unsupervised clustering 
+##          using a personally developed model implementing Spatial Autocorrelation Analysis.
+## Outcome: Discovered large-scale segregation of dopant domains in graphene which led to the discovery of a 
+##          new mechanism of doping.  The autocorrelation model was able to detect an unknown number of clusters, 
+##          unlike logistic regression or SVM in which the number of clusters must be pre-specified.  
+##          The model also achieved detection of nonlinear domain boundaries.  
+##          Results are published in [JACS 136, 1391 (2014)](http://dx.doi.org/10.1021/ja408463g).  
 
 ## 0. Background
-## The purpose of this study is to analyze the patterns of dopant distribution in graphene, resulting from various doping methods, in order to understand and better control the doping process.  Two doping methods were developed: (1) chemical vapor deposition (_Sample 1_), and (2) reaction with ammonia (_Sample 2_).  
-## Data consists of dopant coodinates in nanometers, and sublattice positions with regards to graphene's unit cell (_A_ or _B_).  The goal is to detect the clustering of dopants with respect to the sublattice position.  Note that in pristine graphene the sublattice positions are equivalent, therefore they are expected to be uniformly occupied by the dopants.
+## The purpose of this study is to analyze the patterns of dopant distribution in graphene, resulting from 
+## various doping methods, in order to understand and better control the doping process.  Two doping methods
+## were developed: (1) chemical vapor deposition (Sample 1), and (2) reaction with ammonia (Sample 2). 
+## Data consists of dopant coodinates (in nm), and sublattice positions with regards to graphene's unit cell (A or B).  
+## The goal is to detect the clustering of dopants with respect to the sublattice position.  
+## Note that in pristine graphene the sublattice positions are equivalent, therefore they are expected to be 
+## uniformly occupied by the dopants.
 
 
 ## 1. Data Visualization
@@ -23,20 +33,15 @@ from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 
-## Set the maximum number of rows for displaying tables
-pd.options.display.max_rows = 5
-
+## Load the data
 sample1 = pd.read_csv('sample1.csv')
 sample2 = pd.read_csv('sample2.csv')
 sample1
 
-
-# In[2]:
-
 fig, (ax1, ax2) = plt.subplots(1,2)
 fig.set_figwidth(10)
 
-# Plot Sample1
+## Plot Sample1
 gp = sample1.groupby('Sublattice')
 gp.get_group('A').plot(x='X', y='Y', style='b^', ax=ax1)
 gp.get_group('B').plot(x='X', y='Y', style='rv', ax=ax1)
@@ -45,7 +50,7 @@ ax1.set_xlabel('X'); ax1.set_ylabel('Y')
 ax1.set_xlim(0,100); ax1.set_ylim(100,0)
 ax1.axis('image')
 
-# Plot Sample2
+## Plot Sample2
 gp = sample2.groupby('Sublattice')
 gp.get_group('A').plot(x='X', y='Y', style='b^', ax=ax2)
 gp.get_group('B').plot(x='X', y='Y', style='rv', ax=ax2)
@@ -57,14 +62,9 @@ plt.legend(['A','B'], bbox_to_anchor=(1.35,0.65), title='Sublattice')
 plt.show()
 
 
-# In Sample 1, the red and blue dopants seem to be well segregated, whereas in Sample 2 the dopant distribution appears to be more disordered.  The objective is to identify the sizes of the clustering domains as well as the domain boundaries.
+## 2. Feature Normalization
 
-### 2. Feature Normalization
-
-# Rescale the coordinates to the [0,1] range, and create binary values for the sublattices:
-
-# In[3]:
-
+## Rescale the coordinates to the [0,1] range, and create binary values for the sublattices:
 def normalize(data):
     data['x'] = (data.X - data.X.min()) / (data.X.max() - data.X.min())
     data['y'] = (data.Y - data.Y.min()) / (data.Y.max() - data.Y.min())
@@ -73,13 +73,9 @@ def normalize(data):
 
 normalize(sample1)
 normalize(sample2)
-sample1
 
 
-### 3. Logistic Regression
-
-# In[4]:
-
+## 3. Logistic Regression
 def LR(data, cost=1.0):
     X = np.matrix([data.x, data.y]).T
     y = data.Type
@@ -113,12 +109,7 @@ ax=ax2; LR(sample2); ax.set_title('Sample 2')
 plt.show()
 
 
-# For Sample 1, logistic regression can identify a linear boundary between the red and blue domains since only two clusters are present.  But for Sample 2 it fails to correctly identify the clusters.
-
-### 4. Support Vector Machines
-
-# In[5]:
-
+## 4. Support Vector Machines
 def SVM(data, cost=1.0):
     X = np.matrix([data.x, data.y]).T
     y = data.Type
@@ -135,13 +126,11 @@ ax=ax2; SVM(sample2); ax.set_title('Sample 2')
 plt.show()
 
 
-# SVM provides slight improvement to logistic regression -- it can detect a slighltly nonlinear domain boundary in Sample 1 -- but for Sample 2 it too performs poorly.
-
-### 5. Unsupervised Clustering: Spatial Autocorrelation
-
-# We have developed a nonlinear clustering model utilizing autocorrelation analysis, in which the local densities of dopants in either sublattice are calculated at every point of the map.  From the ratios of the local densities, the degree of segregation (purity of cluster) is computed, which in turn is used to evaluate the cluster sizes.
-
-# In[6]:
+## 5. Unsupervised Clustering: Spatial Autocorrelation
+## We have developed a nonlinear clustering model utilizing autocorrelation analysis, 
+## in which the local densities of dopants in either sublattice are calculated at every point of the map.  
+## From the ratios of the local densities, the degree of segregation (purity of cluster) is computed, 
+## which in turn is used to evaluate the cluster sizes.
 
 def AutoCorr(data, alpha=2):
     # Create a grid map
@@ -193,16 +182,13 @@ ax=ax2; AutoCorr(sample2); ax.set_title('Sample 2')
 plt.show()
 
 
-# The colormap shows the segregation of dopants at every point of the map.  The segregation ranges from 50% (even distribution) to 100% (complete segregation).  Dark red and blue colors indicate clusters where either sublattice is dominant.  White indicates domain boundary.  
+## Parameter Optimization: Overfitting vs. Underfitting
+## The only parameter for the model is  `alpha`  which is a decay constant for the exponential downweighting.  
+## Larger  `alpha`  corresponds to less downweighting, which results in including a larger radius when 
+## calculating the local densities.
 
-### Parameter Optimization: Overfitting vs. Underfitting
 
-# The only parameter for the model is  `alpha`  which is a decay constant for the exponential downweighting.  Larger  `alpha`  corresponds to less downweighting, which results in including a larger radius when calculating the local densities.
-
-###### alpha = 1
-
-# In[7]:
-
+## alpha = 1
 fig, (ax1, ax2) = plt.subplots(1,2)
 fig.set_figwidth(10)
 ax=ax1; AutoCorr(sample1, 1); ax.set_title('Sample 1')
@@ -210,10 +196,7 @@ ax=ax2; AutoCorr(sample2, 1); ax.set_title('Sample 2')
 plt.show()
 
 
-###### alpha = 3
-
-# In[8]:
-
+## alpha = 3
 fig, (ax1, ax2) = plt.subplots(1,2)
 fig.set_figwidth(10)
 ax=ax1; AutoCorr(sample1, 3); ax.set_title('Sample 1')
@@ -221,9 +204,11 @@ ax=ax2; AutoCorr(sample2, 3); ax.set_title('Sample 2')
 plt.show()
 
 
-# - Small `alpha` could lead to overfitting.
-# - Large `alpha` could lead to underfitting.
+## Small `alpha` could lead to overfitting.
+## Large `alpha` could lead to underfitting.
 
-### Conclusion
 
-# Our analysis shows that samples prepared by method 1 exhibit a large degree of dopant clustering (~ 85 - 90% segregation), whereas samples produced by method 2 are essentially disordered (~ 60% segregation) and cluster sizes are much smaller.
+## Conclusion
+## Our analysis shows that samples prepared by method 1 exhibit a large degree of dopant clustering 
+## (~ 85 - 90% segregation), whereas samples produced by method 2 are essentially disordered 
+## (~ 60% segregation) and cluster sizes are much smaller.
