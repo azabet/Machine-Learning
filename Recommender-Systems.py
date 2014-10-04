ec6c1f25f103cbb94b12a74b59bccd1da4ddbb97
azabet <azabet@gmail.com>
2014-10-04 09:05:05 -0400

    Update Recommender-Systems.py

--- Recommender-Systems.py
+++ Recommender-Systems.py
@@ -14,13 +14,7 @@
 # 2. <u>_k-Nearest Neightbors_</u> (kNN):  Calculate the similarity between users based on their ratings on common movies.  Then, take the average rating of nearest-neightbors to predict the ratings of unrated movies.  This method does not require knowledge of movie features.
 # 3. <u>_Collaborative Filtering_</u>: Extract both the movie features and user preferences directly from the data by concurrently optimizating the parameters through _linear regression_. 
 
-#######  
-
-#### Initialize
-
-# In[1]:
-
-get_ipython().magic(u'reset -f')
+## Initialize
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
@@ -30,140 +24,92 @@ from sklearn.svm import SVR
 from sklearn.cross_validation import KFold
 
 
-#### Explore the data
-
-# In[2]:
-
+## Explore the data
 data = pd.read_table('u.data').drop('timestamp', axis=1)
 N = len(data)
 print data.shape
-data.head(2)
+print data.head(2)
 
 
-####### Number of users:
-
-# In[3]:
-
+## Number of users:
 Nu = len(data.userID.unique())
-Nu
+print Nu
 
 
-####### Number of movies:
-
-# In[4]:
-
+## Number of movies:
 Nm = len(data.movieID.unique())
-Nm
+print Nm
 
 
-####### Rating distribution:
-
-# In[5]:
-
+## Rating distribution:
 data.rating.value_counts(sort=False).plot(kind='bar')
 plt.xlabel('Rating')
 plt.ylabel('Count')
 plt.show()
 
 
-####### Number of movies per users:
-
-# In[6]:
-
+## Number of movies per users:
 movies_per_users = data.userID.value_counts()
 print 'min = %d, mean = %d, max = %d' %(movies_per_users.min(), 
                                         movies_per_users.mean(),
                                         movies_per_users.max())
 
 
-####### Number of users per movies:
-
-# In[7]:
-
+## Number of users per movies:
 users_per_movies = data.movieID.value_counts()
 print 'min = %d, mean = %d, max = %d' %(users_per_movies.min(), 
                                         users_per_movies.mean(),
                                         users_per_movies.max())
 
 
-####### Convert data to matrix
-
-# In[8]:
-
+## Convert data to matrix
 rating = data.pivot(index='userID', columns='movieID').rating
-print rating.shape
-rating.head(2)
-
-
-# In[9]:
-
 userID = rating.index
 movieID = rating.columns
+print rating.shape
+print rating.head(2)
 
-
-#######  
 
 ## 1. Content-Based Filtering
 
-####### Load user information
-
-# In[10]:
-
+## Load user information
 users_info = pd.read_table('u.user', sep='|').set_index('userID')
 print users_info.shape
-users_info.head(2)
+print users_info.head(2)
 
 
-####### Create age groups
-
-# The age bins are adapted from the IMDb website.
-
-# In[11]:
-
+## Create age groups
+## The age bins are adapted from the IMDb website.
 users_info['age_group'] = pd.cut(users_info.age, [0,18,29,45,np.inf])
 users_info.drop(['age', 'occupation', 'zipcode'], axis=1, inplace=True)
-users_info.head(2)
+print users_info.head(2)
 
 
-####### Load movie information
-
-# In[12]:
-
+## Load movie information
 movies_info = pd.read_table('u.item', sep='|').set_index('movieID')#.drop(low_count_movies)
 movies_info['release date'] = pd.to_datetime(movies_info['release date'])
 print movies_info.shape
-movies_info.head(2)
+print movies_info.head(2)
 
 
-####### Join data together
-
-# In[13]:
-
+## Join data together
 joined = data.join(users_info, on='userID').join(movies_info.iloc[:,4:], on='movieID')
 joined.sort(['userID', 'movieID'], inplace=True)
 print joined.shape
-joined.head(2)
+print joined.head(2)
 
 
-####### Calculate average ratings by gender and age group
-
-# The average rating will be used as an additional movie parameter.
-
-# In[14]:
-
+## Calculate average ratings by gender and age group
+## The average rating will be used as an additional movie parameter.
 avg = joined.groupby(['movieID', 'gender', 'age_group']).rating.mean()
 newdata = joined.join(avg, rsuffix='_avg', on=['movieID', 'gender', 'age_group'])
 newdata.drop(['gender', 'age_group'], axis=1, inplace=True)
 newdata.set_index(['userID', 'movieID'], inplace=True)
-newdata.head(2)
+print newdata.head(2)
 
 
-### Support Vector Machines (SVM)
-
-# Apply SVM to model the ratings of each user based on the movie parameters.
-
-# In[15]:
-
+## Support Vector Machines (SVM)
+## Apply SVM to model the ratings of each user based on the movie parameters.
 def SVM(Xtrain, ytrain, Xtest=None, C=1):
     model = SVR(C=C)  ## module imported from Scikit-Learn
     model.fit(Xtrain, ytrain)
@@ -178,27 +124,16 @@ X = newdata.drop('rating', axis=1)
 Y = newdata.rating
 
 
-####### Evaluation metric: root-mean-squared error (RMSE)
-
-# In[16]:
-
+## Evaluation metric: root-mean-squared error (RMSE)
 def rmse(y, yhat):
     e2 = (y - yhat) ** 2
     return np.sqrt(e2.mean())
 
 
-### k-Fold Cross-Validation
-
-# Optimize the regularization parameter (C) by cross-validation.  The purpose of regularization is to minimize overfitting/underfitting (bias/variance trade off).
-
-# In[31]:
-
+## k-Fold Cross-Validation
+## Optimize the regularization parameter (C) by cross-validation.  The purpose of regularization is to minimize overfitting/underfitting (bias/variance trade off).
 cost = [0.1, 0.3, 1, 3, 10, 30]
 error, CVerror = [], []
-## Pick 200 random users
-#users = list(userID)
-#np.random.shuffle(users)
-#users = users[:200]
 for C in cost:
     for user in userID:
         x, y = X.ix[user], Y.ix[user]
@@ -217,19 +152,7 @@ for C in cost:
     print C, '\t', err_avg, '\t', CVerr_avg
 
 
-####### Plot the training and cross-validation errors
-
-# In[18]:
-
-cost = [0.1, 0.3, 1, 3, 10, 30]
-error = [1.01637306285, 0.932654553545, 0.877025797715, 0.850895183488, 
-         0.824033786882, 0.777168617149]
-CVerror = [1.0422823422, 0.962722662811, 0.929758406982, 0.959016550869, 
-           0.952583414049, 1.01403949125]
-
-
-# In[19]:
-
+## Plot the training and cross-validation errors
 ax = plt.gca()
 plt.plot(cost, error, '-o')
 plt.plot(cost, CVerror, '-o', color='r')
@@ -241,12 +164,10 @@ plt.title('SVM Optimization')
 plt.show()
 
 
-# C = 1 gives the lowest cross-validation error.
+## C = 1 gives the lowest cross-validation error.
 
-####### Plot the predictions
 
-# In[20]:
-
+## Plot the predictions
 def jitter(y, yhat, title=''):
     pred = np.round(yhat)
     pred[pred > 5] = 5
@@ -268,14 +189,9 @@ yhat = SVM(x, y, C=1)
 jitter(y, yhat, 'Content-Based Filtering')
 
 
-#######  
-
 ## 2. k-Nearest Neighbors (kNN)
 
-####### Calculate pairwise distances between users
-
-# In[21]:
-
+## Calculate pairwise distances between users
 def dist(i,j):
     ## Only consider users who have at least 3 movies in common
     d = (rating.ix[i] - rating.ix[j])**2
@@ -291,13 +207,9 @@ for user1 in userID:
     for user2 in userID[userID > user1]:
         D[user1].ix[user2] = dist(user1, user2)
         D[user2].ix[user1] = D[user1].ix[user2]
-    break
 
 
-####### Predict ratings by the average of nearest neighbors
-
-# In[22]:
-
+## Predict ratings by the average of nearest neighbors
 def kNN_predict(user, k=5):
     ## Sort users by distance
     neighbors = D[user].order().index
@@ -311,10 +223,7 @@ def kNN_predict(user, k=5):
     return pred
 
 
-####### Optimize k
-
-# In[86]:
-
+## Optimize k
 user = 1
 K = [3, 5, 10, 15]
 error = []
@@ -331,32 +240,22 @@ plt.ylabel('Error (RMSE)')
 plt.show()
 
 
-# k = 5 gives the lowest error.
+## k = 5 gives the lowest error.
 
-####### Plot the predictions
 
-# In[24]:
-
+## Plot the predictions
 user = 1
 pred = kNN_predict(user, k=5)
 jitter(rating.ix[user], pred, 'kNN')
 
 
-#######  
-
 ## 3. Collaborative Filtering
 
-# In this method, we apply linear regression to fit both the user parameters, _U_, and the movie features, _M_.  We first initialize them to small random values, then fit _U_ and _M_ recursively while keeping the other constant.  In order to have enough data points for the linear regression, we only include movies that have at least 3 ratings.
-
-# In[26]:
-
+## In this method, we apply linear regression to fit both the user parameters, _U_, and the movie features, _M_.  We first initialize them to small random values, then fit _U_ and _M_ recursively while keeping the other constant.  In order to have enough data points for the linear regression, we only include movies that have at least 3 ratings.
 low_count_movies = users_per_movies[users_per_movies < 3].index
 newdata = data.set_index('movieID').drop(low_count_movies).reset_index()
 print newdata.shape
-N = len(newdata)
-
-
-# In[27]:
+print N = len(newdata)
 
 rating = newdata.pivot(index='userID', columns='movieID').rating
 userID, movieID = rating.index, rating.columns
@@ -364,9 +263,7 @@ Nu, Nm = len(userID), len(movieID)
 print Nu, Nm
 
 
-####### Initialize the parameters
-
-# In[26]:
+## Initialize the parameters
 
 ## number of movie features
 n = 20
@@ -378,10 +275,7 @@ U = pd.DataFrame(np.random.rand(Nu, n), index=userID, columns=range(1, n+1))
 M = pd.DataFrame(np.random.rand(Nm, n), index=movieID, columns=range(1, n+1))
 
 
-####### Fit user preferences
-
-# In[27]:
-
+## Fit user preferences
 def fitU():  
     print 'Fitting user parameters',
     
@@ -426,10 +320,7 @@ def predict(U, M):
     return pred, error
 
 
-####### Fit movie features
-
-# In[28]:
-
+## Fit movie features
 def fitM():
     print 'Fitting movie features',
     
@@ -466,10 +357,7 @@ def fitM():
     return delta
 
 
-####### Fit both U and M
-
-# In[209]:
-
+## Fit both U and M
 error_history = []
 bestFit = {'U': U, 'M': M, 'error': np.inf}
 delta = fitU()
@@ -479,31 +367,18 @@ while delta > tolerance:
     if delta > tolerance:
         delta = fitU()
         
-#error_history = np.load('error_history.npy')[:100]
 plt.plot(error_history, '-o')
 plt.xlabel('Iteration')
 plt.ylabel('Error (RMSE)')
 plt.show()
 
 
-# In[28]:
-
-U = pd.DataFrame(np.load('U.npy'), index=userID)
-M = pd.DataFrame(np.load('M.npy'), index=movieID)
-
-
-####### Plot the predictions
-
-# In[29]:
-
+## Plot the predictions
 user = 1
 jitter(rating.ix[user], U.ix[user].dot(M.T), 'Collaborative Filtering')
 
 
-####### Movie recommendations
-
-# In[31]:
-
+## Movie recommendations
 def recommend(user):
     unrated_movies = rating.ix[user][rating.ix[user].isnull()].index
     pred, error = predict(U.ix[user], M.ix[unrated_movies])
@@ -515,8 +390,6 @@ def recommend(user):
 
 recommend(1)
 
-
-# Recommended movies are sorted by the release date, newest first.
 
 ## Conclusions
 
